1dc6d9e7f7ba3f387aac83e9170a09b0cec7d69f
Ashutosh Kumbhar <60011389+2727-ask@users.noreply.github.com>
2026-08-19 01:24:33 -0700

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        def isValid(curr):
+            check = 0
+            for x in curr:
+                if(x == ")"):
+                    check = check - 1
+                else:
+                    check = check + 1
+                if(check < 0):
+                    return False
+            if(check == 0):
+                return True
+            return False
+        res = []
+        def dfs(curr):
+            if(len(curr) == 2*n):
+                if(isValid(curr)):
+                   res.append(curr)
+                return 
+
+
+            curr = curr + "("
+
+            dfs(curr)
+
+            curr = curr[:-1]
+
+            curr = curr + ")"
+
+            dfs(curr)
+        
+        dfs("")
+
+        return res
+
+
